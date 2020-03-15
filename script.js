e868738fd9af3715ea99d1c6269cb577fb33f7b8
Andrei Shablyka <an.shablyka@gmail.com>
2020-03-15 05:20:36 +0300

    feat: create slider

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Navigation
 const navigationElement = document.querySelector("#navigation");
 const navigationItemsElements = navigationElement.querySelectorAll(
   ".navigation__item"
@@ -10,4 +11,25 @@ navigationElement.addEventListener("click", event => {
   event.target
     .closest(".navigation__item")
     .classList.add("navigation__item_active");
+});
+
+// Slider
+const arrowLeftElement = document.querySelector(".arrow-left");
+const arrowRightElement = document.querySelector(".arrow-right");
+const slideElements = document.querySelectorAll(".slide");
+
+let currentSlide = 0;
+
+arrowLeftElement.addEventListener("click", () => {
+  slideElements[currentSlide].classList.remove("slide_active");
+  currentSlide =
+    (currentSlide - 1 + slideElements.length) % slideElements.length;
+  slideElements[currentSlide].classList.add("slide_active");
+});
+
+arrowRightElement.addEventListener("click", () => {
+  slideElements[currentSlide].classList.remove("slide_active");
+  currentSlide =
+    (currentSlide + 1 + slideElements.length) % slideElements.length;
+  slideElements[currentSlide].classList.add("slide_active");
 });
